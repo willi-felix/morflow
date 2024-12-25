0ce99561819f8ff28e87a7227bf07c720ed955ac
William <172239352+willi-felix@users.noreply.github.com>
2024-12-25 17:01:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 app = Flask(__name__)
 
 RAPIDAPI_KEY = os.getenv('RAPIDAPI_KEY')
-RAPIDAPI_HOST = os.getenv('RAPIDAPI_HOST')
+RAPIDAPI_HOST = 'gsamuel-morse-code-v1.p.rapidapi.com'
 
 def is_morse_code(text):
     return all(c in '.- ' for c in text.strip())
@@ -21,21 +21,24 @@ def index():
 def convert():
     text = request.form['text']
     if is_morse_code(text):
-        convert_type = 'from_morse'
+        convert_type = 'decode'
     else:
-        convert_type = 'to_morse'
+        convert_type = 'encode'
 
-    url = f"https://{RAPIDAPI_HOST}/morse.json"
+    url = f"https://{RAPIDAPI_HOST}/{convert_type}"
     headers = {
         'Content-Type': 'application/json',
         'x-rapidapi-host': RAPIDAPI_HOST,
         'x-rapidapi-key': RAPIDAPI_KEY
     }
+    payload = {'text': text}
+    response = requests.post(url, json=payload, headers=headers)
 
-    querystring = {"text": text} if convert_type == 'from_morse' else {"text": text, "speed": "5", "tone": "700"}
-    response = requests.post(url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        result = response.json().get('morse', 'Error in conversion')
+    else:
+        result = 'Error in conversion'
 
-    result = response.json().get('morse', 'Error in conversion')
     return render_template('index.html', result=result, input_text=text)
 
 if __name__ == '__main__':
